55ca47ece471444f41c7f073ccff0624b2745ad4
Shivam Srivastava <shivamsri9211@gmail.com>
2024-09-22 13:22:50 +0530

    I have create enquiry_products table

---
+++ database/migrations/2024_09_22_074958_enquiry_product.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enquiry_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('enquiries_id');
+            $table->foreign('enquiries_id')->references('id')->on('enquiries');
+           $table->unsignedBigInteger('products_id');
+           $table->foreign('products_id')->references('id')->on('products');
+            $table->tinyInteger('status')->command("0 => active, 1 => inactive");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
